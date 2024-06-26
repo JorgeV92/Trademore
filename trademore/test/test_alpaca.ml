65163a774c70dbe08a95afae889da1a89000addb
Jorge Velez <jorgevmg31@gmail.com>
2024-06-26 18:06:21 -0500

    Updated the errors

--- trademore/test/test_alpaca.ml
+++ trademore/test/test_alpaca.ml
@@ -4,4 +4,6 @@ open Trademore.Alpaca
 
 let () = 
   Lwt_main.run (
-    Lwt_io.printf "\nRunning test of account
+    Lwt_io.printf "\nRunning test of account Alpaca access...\n" >>= fun () -> 
+    Alpaca.run_account_info ()
+  )
